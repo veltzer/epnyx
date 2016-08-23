1e4dde34a6c137b9f0d276c00867ecf64a88a3e1
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-23 11:22:20 +0300

    python fixes

--- helloworld.py
+++ helloworld.py
@@ -3,21 +3,21 @@ import jinja2 # for Environment, FileSystemLoader
 import os.path # for dirname
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-	extensions=['jinja2.ext.autoescape'],
-	autoescape=True,
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True,
 )
 
 class MainPage(webapp2.RequestHandler):
-	def get(self):
-		'''
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Hello, World!')
-		'''
-		template_values = {}
-		template = JINJA_ENVIRONMENT.get_template('templates/index.jinja')
-		self.response.write(template.render(template_values))
+    def get(self):
+        '''
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+        '''
+        template_values = {}
+        template = JINJA_ENVIRONMENT.get_template('templates/index.jinja')
+        self.response.write(template.render(template_values))
 
 application = webapp2.WSGIApplication([
-	('/', MainPage),
+    ('/', MainPage),
 ], debug=True)
